8f340310e13697a8b74f16300f868d8dc9009f96
shopvn62 <147729101+shopvn62@users.noreply.github.com>
2023-10-15 21:01:50 +0700

    Add files via upload

---
+++ js/fonts.js
@@ -0,0 +1,18 @@
+function confirmAndRedirect() {
+    var result = confirm("Bạn có muốn mua sản phẩm này không? \n Chuyển đến Messenger");
+    if (result) {
+      // Thay thế "your_facebook_page_url_here" bằng đường dẫn đến trang Facebook của bạn
+      window.location.href = "https://m.me/lemin62";
+    }
+  }
+	
+      // text code lemin.vn 
+document.addEventListener("DOMContentLoaded", function() {
+    var options = {
+      strings: ['When You Write The Story of Your Life', 'Don"t Let Anyone else Hold The Pen', 'Live A Life You Will Remember', 'Cường ReLover'],
+      typeSpeed: 80,
+      backSpeed: 25,
+      loop: true
+    };
+    var typed = new Typed("#lemin-text", options);
+ });
